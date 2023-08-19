3c3d68d2152e4cac5b49e8f0fa31260e673348e9
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-18 18:54:18 -0600

    generate from database

--- CursoEntityCore/Models/Categoria.cs
+++ CursoEntityCore/Models/Categoria.cs
@@ -1,9 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CursoEntityCore.Models
 {
     public class Categoria
     {
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
 
         [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "[NULL]")]
