ce7d595caf00f302884573c99228c30a337a6d0c
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-18 18:09:06 -0600

    add primary key

--- CursoEntityCore/Models/Articulo.cs
+++ CursoEntityCore/Models/Articulo.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CursoEntityCore.Models
 {
     [Table("tbl_Articulo")]
     public class Articulo
     {
-        public int ArticuloId { get; set; }
+        [Key]
+        public int Articulo_Id { get; set; }
 
         [Column("Titulo")]
         public string TituloArticulo { get; set; }
