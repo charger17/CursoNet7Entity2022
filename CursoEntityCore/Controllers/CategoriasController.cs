7c81d5dc130c2713530792fef1fd302afc37ef2a
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-21 16:59:04 -0600

    read records

---
+++ CursoEntityCore/Controllers/CategoriasController.cs
@@ -0,0 +1,23 @@
+﻿using CursoEntityCore.Datos;
+using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEntityCore.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Categoria> listaCategorias = _context.Categorias.ToList();
+
+            return View(listaCategorias);
+        }
+    }
+}
