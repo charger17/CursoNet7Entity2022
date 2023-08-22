cc237f7aac860f1355ddaeeb0ea9ca1caa98f019
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-21 18:17:40 -0600

    remove data

--- CursoEntityCore/Controllers/CategoriasController.cs
+++ CursoEntityCore/Controllers/CategoriasController.cs
@@ -132,5 +132,47 @@ namespace CursoEntityCore.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Borrar(int? id)
+        {
+            if (id is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(c => c.Categoria_Id.Equals(id));
+            if (categoria is null)
+            {
+                return RedirectToAction(nameof(Index));
+
+            }
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BorrarMultiple2()
+        {
+            IEnumerable<Categoria> categorias = _context.Categorias.OrderByDescending(c => c.Categoria_Id).Take(2);
+            _context.Categorias.RemoveRange(categorias);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BorrarMultiple5()
+        {
+            IEnumerable<Categoria> categorias = _context.Categorias.OrderByDescending(c => c.Categoria_Id).Take(5);
+            _context.Categorias.RemoveRange(categorias);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
