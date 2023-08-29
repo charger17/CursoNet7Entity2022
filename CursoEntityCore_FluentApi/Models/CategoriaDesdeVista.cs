54c8560fdb5c1de990761fbd1d3fb90e63709a33
Carlos Perez Salazar <cps172779@gmail.com>
2023-08-28 19:35:37 -0600

    Map View

---
+++ CursoEntityCore_FluentApi/Models/CategoriaDesdeVista.cs
@@ -0,0 +1,14 @@
+﻿namespace CursoEntityCore_FluentApi.Models
+{
+    public class CategoriaDesdeVista
+    {
+        public int Categoria_Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
